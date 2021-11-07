53379505936ec37e6f3c425c4ad4d424a4ca86e1
tomiharu0317 <tomiharu1017@gmail.com>
2021-11-07 14:21:08 +0900

    practice

--- r_practice.R
+++ r_practice.R
@@ -3,7 +3,6 @@ barplot(df$April, names.arg=rownames(df), las=1, main="Sales in April", cex.main
 options(scipen=100)
 par(mar=c(5.1,6.1,5.1,2.1))
 
-
 pie(df$April)
 df2 <- df[order(df$April, decreasing=TRUE),]
 pie(df2$April, clockwise=TRUE, labels=rownames(df2), main="Proportion of sales in April", cex.main=3)
@@ -112,3 +111,94 @@ df <- read.csv("mini-exam5-3.csv")
 df
 summary(aov(removed~brand+temperature, data = df))
 summary(aov(Distance~Golfer+Brand, data = df))
+
+# Rによる単回帰分析
+df <- read.csv("practice6-1.csv")
+df
+lm(df$Brth15to17~df$PovPct)
+anova(lm(Brth15to17~PovPct, data=df))
+summary(lm(Brth15to17~PovPct, data=df))
+
+df <- read.csv("mini-exam6-1.csv")
+df
+lm(df$Accuracy~df$Distance)
+summary(lm(df$Accuracy~df$Distance))
+
+
+# Rでの分岐処理と繰り返し処理
+df <- read.csv("practice6-1.csv")
+df
+# 結果を格納結果を格納
+result <- lm(df$Brth15to17~df$PovPct)
+names(result$coefficients)
+beta_0 <- result$coefficients[1]
+beta_1 <- result$coefficients[2]
+# 推定出生率の計算関数
+birthRate <- function(PovPct) {
+  rate <- beta_0 + (beta_1 * PovPct)
+  return(rate)
+}
+# for を使った計算
+Brth <- df$Brth15to17
+PovPct <- df$PovPct
+count <- 0
+n <- length(Brth)
+
+for (i in 1:n) {
+  povpct <- PovPct[i]
+  rate_actual <- Brth[i]
+  rate_estimated <- birthRate(povpct)
+  if (rate_actual > rate_estimated) {
+    count <- count + 1
+  }
+}
+
+print(count)
+
+# for を使わないで計算する
+estimated_rate <- c(PovPct*beta_1 + beta_0)
+sum(Brth > estimated_rate)
+
+# Rによるシミュレーション実験
+n_exp <- 10000 #実験回数
+n_trial <- 1000　#コイン投げの回数
+count <- 0
+for (i in 1:n_exp) {
+  unif_01 <- runif(n_trial, 0, 1)
+  coin_flip <- unif_01 < 0.5
+  if (sum(coin_flip) > 550) {
+    count <- count + 1
+  }
+}
+
+count / n_exp
+
+# Rによるシミュレーション実験(2)
+n_exp <- 10000
+n_trial <- 1000
+mean <- 0
+sd <- 1
+count <- 0
+for (i in 1:n_exp) {
+  sample <- rnorm(n_trial, mean, sd)
+  x_hat <- mean(sample)
+  mu_lower <- x_hat - 1.96 * (sqrt(sd / n_trial))
+  mu_upper <- x_hat + 1.96 * (sqrt(sd / n_trial))
+  if (mu_lower < mean && mean < mu_upper) {
+    count <- count + 1
+  }
+}
+
+count / n_exp
+
+
+
+
+
+
+
+
+
+
+
+
